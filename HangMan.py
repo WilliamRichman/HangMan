df95349ed27d14a738b68b4bf0981f6f32d7a198
WilliamRichman <Goat1120@gmail.com>
2024-01-08 12:25:14 -0500

    Added the word list and the ascii pics

---
+++ HangMan.py
@@ -0,0 +1,47 @@
+#HangMan 1/8/2024
+
+
+import random
+HANGMAN_PICS = [
+    
+    '''
+    +---+
+        |
+        |
+        |
+       === ''','''
+    +---+
+    0   |
+        |
+        |
+       === ''', '''
+    +---+
+    0   |
+    |   |
+        |
+       === ''','''
+    +---+
+    0   |
+   /|   |
+        |
+       === ''','''
+    +---+
+    0   |
+   /|\  |
+        |
+       === ''', '''
+    +---+
+    0   |
+   /|\  |
+   /    |
+       === ''', '''
+    +---+
+    0    |
+   /|\   |
+   / \  |
+       === ''']
+
+words = 'ant babboon badger bat beaver bear camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferret for frog goat goose hawk lion lizard llama mole monkey moose mouse mule newt otter owl panda parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake spide stork swan tiger toad trout turcky turtle weasel whale wolf wombat zebra'.split()
+
+
+
